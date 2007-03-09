d8a2a093b92dce7257ac43b45d71bc8bea46ae36
venthur <>
2007-03-09 15:59:29 +0000

    Major refactoring.

---
+++ src/lib/ReportbugNG.py
@@ -0,0 +1,53 @@
+import commands
+
+VERSION = "0.2007.03.08"
+
+
+def prepareMail(mua, text):
+    """Tries to call MUA with the text (mailto-format)"""
+    
+    command = str(mua + " " + text.encode("ascii", "replace"))
+    if not commands.getstatusoutput(command)[0] == 0:
+        print "Reportbug was not able to start your mailclient."
+        print "Please copy-paste the following text to your mailclient and send it to submit@bugs.debian.org"
+        print text
+    
+    
+def createMailtoString(to, subject, package, version, severity=None):
+    """Creates a Mailto-Line containing the whole email plus some sysinfo."""
+    
+    s = "mailto:%s?subject=%s" % (to, subject)
+    
+    s += "&body=" 
+    s += "Package: %s\n" % package
+    s += "Version: %s\n" % version 
+
+    if severity:
+        s += "Severity: %s\n" % severity
+    
+    s += "\n"
+    s += "--- Please enter the report below this line. ---\n\n\n"
+
+    #s self.getSystemInformation()
+            
+    s = s.replace("\n", "%0D%0A")
+    s = "\"" + s + "\""
+    
+    return s
+
+def getSystemInformation():
+    """Returns some hopefully usefull sysinfo"""
+    
+    s = "--- System infomation. ---\n"
+    
+    list = ("dpkg --print-installation-architecture", 
+            "uname -srv",
+            "uname -mpio",
+            "apt-cache policy | grep Packages$")
+    
+    for cmd in list:
+        s += "%s:\n" % cmd
+        s += commands.getoutput("%s 2>/dev/null" % cmd)
+        s += "\n\n"
+    
+    return s
--- src/ui/MyMainWindow.py
+++ src/ui/MyMainWindow.py
@@ -1,9 +1,9 @@
 from MainWindow import Form
 from SubmitDialog import SubmitDialog
-from bts.Bugreport import Bugreport
-from bts.BTS import BTS
+from lib.Bugreport import Bugreport
+from lib import DebianBTS
+from lib.ReportbugNG import *
 
-import commands
 
 class MyMainWindow(Form):
     
@@ -19,7 +19,7 @@ class MyMainWindow(Form):
     
     
     def lineEdit_returnPressed(self):
-        """The user changed the text in the combobox and pressed enter"""
+        """The user changed the text in the combobox and pressed enter."""
         
         self.currentPackage = unicode(self.lineEdit.text())
         self.textBrowser.setText("Fetching bugreports for package %s, please wait." % self.currentPackage)
@@ -28,7 +28,7 @@ class MyMainWindow(Form):
     
         self.bugs = []
         
-        self.bugs = BTS().getBugsByPackage(self.currentPackage)
+        self.bugs = DebianBTS.getBugsByPackage(self.currentPackage)
         self.visibleBugs = self.bugs
 
         self.listBox.clear()
@@ -42,7 +42,7 @@ class MyMainWindow(Form):
         
 
     def lineEdit_textChanged(self, a0):
-        """Text in Filter has changed"""
+        """The filter text has changed."""
         
         self.visibleBugs = []
         self.listBox.clear()
@@ -54,7 +54,7 @@ class MyMainWindow(Form):
 
         
     def listBox_highlighted(self,a0):
-        """The user selected a Bug from the list"""
+        """The user selected a Bug from the list."""
         
         self.listBox.update()
         self.pushButtonAdditionalInfo.setEnabled(1)
@@ -63,13 +63,13 @@ class MyMainWindow(Form):
         
         # Fetch the fulltext if not yet available.
         if len(self.visibleBugs[a0].fulltext) == 0:
-            self.visibleBugs[a0].fulltext = BTS().getFullText(self.visibleBugs[a0].nr)
+            self.visibleBugs[a0].fulltext = DebianBTS.getFullText(self.visibleBugs[a0].nr)
         
         self.textBrowser.setText(self.visibleBugs[a0].fulltext)
         
     
     def pushButtonAdditionalInfo_clicked(self):
-        """The user wants to provide additional info to current bug"""
+        """The user wants to provide additional info for the current bug."""
     
         summary = ""
         mua = ""
@@ -92,13 +92,13 @@ class MyMainWindow(Form):
             version = dialog.lineEditVersion.text()
             to = "%s@bugs.debian.org" % self.currentBug.nr
             
-            self.prepareMail(mua, self.createMailtoString(to, subject, package, version))
+            prepareMail(mua, createMailtoString(to, subject, package, version))
         else:
             pass
 
     
     def pushButtonNewBugreport_clicked(self):
-        """The User wants to file a new bugreport against the current package"""
+        """The User wants to file a new bugreport against the current package."""
         
         summary = ""
         severity = ""
@@ -123,57 +123,8 @@ class MyMainWindow(Form):
             version = dialog.lineEditVersion.text()
             to = "submit@bugs.debian.org"
             
-            self.prepareMail(mua, self.createMailtoString(to, subject, package, version, severity))
+            prepareMail(mua, createMailtoString(to, subject, package, version, severity))
         else:
             pass
 
 
-    def prepareMail(self, mua, text):
-        """Tries to call mua with the text (mailto-format)"""
-        
-        command = str(mua + " " + text.encode("ascii", "replace"))
-        if not commands.getstatusoutput(command)[0] == 0:
-            print "Reportbug was not able to start your mailclient."
-            print "Please copy-paste the following text to your mailclient and send it to submit@bugs.debian.org"
-            print text
-        
-        
-    def createMailtoString(self, to, subject, package, version, severity=None):
-        """Creates a Mailto-Line containing the whole email plus some sysinfo."""
-        
-        s = "mailto:%s?subject=%s" % (to, subject)
-        
-        s += "&body=" 
-        s += "Package: %s\n" % package
-        s += "Version: %s\n" % version 
-
-        if severity:
-            s += "Severity: %s\n" % severity
-        
-        s += "\n"
-        s += "--- Please enter the report below this line. ---\n\n\n"
-
-        #s self.getSystemInformation()
-                
-        s = s.replace("\n", "%0D%0A")
-        s = "\"" + s + "\""
-        
-        return s
-
-    def getSystemInformation(self):
-        """Returns some hopefully usefull sysinfo"""
-        
-        s = "--- System infomation. ---\n"
-        
-        list = ("dpkg --print-installation-architecture", 
-                "uname -srv",
-                "uname -mpio",
-                "apt-cache policy | grep Packages$")
-        
-        for cmd in list:
-            s += "%s:\n" % cmd
-            s += commands.getoutput("%s 2>/dev/null" % cmd)
-            s += "\n\n"
-        
-        return s
-        
