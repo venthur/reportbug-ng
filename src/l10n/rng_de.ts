6d6b0adc5bbf8bfb8b6e5aa665413ec38e785e6e
Bastian Venthur <venthur@debian.org>
2009-10-24 13:56:28 +0200

    Prepared for QT's l10n stuff.

---
+++ src/l10n/rng_de.ts
@@ -0,0 +1,297 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>Dialog</name>
+    <message>
+        <source>Settings</source>
+        <translation>Einstellungen</translation>
+    </message>
+    <message>
+        <source>External Applications</source>
+        <translation>Externe Anwendungen</translation>
+    </message>
+    <message>
+        <source>Mail Client:</source>
+        <translation>Mail Programm:</translation>
+    </message>
+    <message>
+        <source>User Interface</source>
+        <translation>Benutzerinterface</translation>
+    </message>
+    <message>
+        <source>Critical:</source>
+        <translation>Kritisch:</translation>
+    </message>
+    <message>
+        <source>Grave:</source>
+        <translation>Gravierend:</translation>
+    </message>
+    <message>
+        <source>Serious:</source>
+        <translation>Ernsthaft:</translation>
+    </message>
+    <message>
+        <source>Important:</source>
+        <translation>Wichtig:</translation>
+    </message>
+    <message>
+        <source>Normal:</source>
+        <translation>Normal:</translation>
+    </message>
+    <message>
+        <source>Minor:</source>
+        <translation>Kleinigkeit:</translation>
+    </message>
+    <message>
+        <source>Wishlist:</source>
+        <translation>Wunschliste:</translation>
+    </message>
+    <message>
+        <source>Resolved:</source>
+        <translation>Gelöst:</translation>
+    </message>
+    <message>
+        <source>Miscellaneous</source>
+        <translation>Verschiedenes</translation>
+    </message>
+    <message>
+        <source>Send additional bugscript information if available (recommended)</source>
+        <translation>Sende zusätzliche bugscript-Informationen falls möglich (empfohlen)</translation>
+    </message>
+    <message>
+        <source>Show package specific information before sending bugreport if available (recommended)</source>
+        <translation>Zeige paketspezifische Informationen, falls möglich, bevor der Fehlerbericht gesendet wird (emfohlen)</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>Reportbug NG</source>
+        <translation>Reportbug NG</translation>
+    </message>
+    <message>
+        <source>Q&amp;uery or Filter:</source>
+        <translation>Q&amp;uery oder Filter:</translation>
+    </message>
+    <message>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+    <message>
+        <source>&amp;File</source>
+        <translation>&amp;Datai</translation>
+    </message>
+    <message>
+        <source>&amp;Bugs</source>
+        <translation>&amp;Fehler</translation>
+    </message>
+    <message>
+        <source>&amp;Edit</source>
+        <translation type="unfinished">B&amp;earbeiten</translation>
+    </message>
+    <message>
+        <source>&amp;Help</source>
+        <translation type="unfinished">&amp;Hilfe</translation>
+    </message>
+    <message>
+        <source>toolBar</source>
+        <translation>Symbolleiste</translation>
+    </message>
+    <message>
+        <source>&amp;New Bugreport...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Create a new Bugreport for the current package.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Ctrl+N</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Additional Information...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Provide additional information for this Bugreport.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Ctrl+A</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Close Bugreport...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Close the current Bugreport.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Ctrl+L</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>New &amp;WNPP Bugreport...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Create a new WNPP Bugreport.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Ctrl+W</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Clear</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Clear.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Quit Reportbug-NG.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Ctrl+Q</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Change Reportbug-NG&apos;s settings.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>About Reportbug NG</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>About Qt</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>RngGui</name>
+    <message>
+        <source>About Reportbug-NG</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Copyright (C) 2007-2009 Bastian Venthur &lt;venthur at debian org&gt;
+
+Homepage: http://reportbug-ng.alioth.debian.org
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>About Qt</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SubmitDialog</name>
+    <message>
+        <source>Bug report details</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Please add details about the bug report you want to report.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Package:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Version:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;WNPP Bugreport</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Short Package &amp;Description:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>WNPP &amp;Action:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Normal Bugreport</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&lt;b&gt;Please enter a short summary. Severity and Tags are optional.&lt;/b&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Summary:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>S&amp;everity:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Lorem Ipsum. Lorem Ipsum. Lorem Ipsum. Lorem Ipsum. Lorem Ipsum. Lorem Ipsum. Lorem Ipsum. Lorem Ipsum. Lorem Ipsum. Lorem Ipsum. Lorem Ipsum. Lorem Ipsum. Lorem Ipsum. Lorem Ipsum. Lorem Ipsum. Lorem Ipsum. Lorem Ipsum. Lorem Ipsum. Lorem Ipsum. Lorem Ipsum.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Tags:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>A patch or some other easy procedure for fixing the bug is included in the bug logs. If there&apos;s a patch, but it doesn&apos;t resolve the bug adequately or causes some other problems, this tag should not be used.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>patch</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>This bug is relevant to the localisation of the package.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>l10n</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>This bug describes a security problem in a package (e.g., bad permissions allowing access to data that shouldn&apos;t be accessible; buffer overruns allowing people to control a system in ways they shouldn&apos;t be able to; denial of service attacks that should be fixed, etc). Most security bugs should also be set at critical or grave severity.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>security</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Miscellaneous</source>
+        <translation type="unfinished">Verschiedenes</translation>
+    </message>
+    <message>
+        <source>Send additional bugscript information if available (recommended)</source>
+        <translation type="unfinished">Sende zusätzliche bugscript-Informationen falls möglich (empfohlen)</translation>
+    </message>
+    <message>
+        <source>Show package specific information before sending bugreport if available (recommended)</source>
+        <translation type="unfinished">Zeige paketspezifische Informationen, falls möglich, bevor der Fehlerbericht gesendet wird (emfohlen)</translation>
+    </message>
+</context>
+</TS>
---
+++ src/l10n/rng_foo.ts
@@ -0,0 +1,367 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1">
+<context>
+    <name>Dialog</name>
+    <message>
+        <location filename="settings.py" line="102"/>
+        <source>Settings</source>
+        <translation>Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="settings.py" line="103"/>
+        <source>External Applications</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.py" line="104"/>
+        <source>Mail Client:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.py" line="105"/>
+        <source>User Interface</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.py" line="106"/>
+        <source>Critical:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.py" line="107"/>
+        <source>Grave:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.py" line="108"/>
+        <source>Serious:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.py" line="109"/>
+        <source>Important:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.py" line="110"/>
+        <source>Normal:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.py" line="111"/>
+        <source>Minor:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.py" line="112"/>
+        <source>Wishlist:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.py" line="113"/>
+        <source>Resolved:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.py" line="114"/>
+        <source>Miscellaneous</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.py" line="115"/>
+        <source>Send additional bugscript information if available (recommended)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.py" line="116"/>
+        <source>Show package specific information before sending bugreport if available (recommended)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.py" line="130"/>
+        <source>Reportbug NG</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.py" line="131"/>
+        <source>Q&amp;uery or Filter:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.py" line="132"/>
+        <source>...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.py" line="133"/>
+        <source>&amp;File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.py" line="134"/>
+        <source>&amp;Bugs</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.py" line="135"/>
+        <source>&amp;Edit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.py" line="136"/>
+        <source>&amp;Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.py" line="137"/>
+        <source>toolBar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.py" line="138"/>
+        <source>&amp;New Bugreport...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.py" line="139"/>
+        <source>Create a new Bugreport for the current package.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.py" line="140"/>
+        <source>Ctrl+N</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.py" line="141"/>
+        <source>&amp;Additional Information...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.py" line="142"/>
+        <source>Provide additional information for this Bugreport.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.py" line="143"/>
+        <source>Ctrl+A</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.py" line="144"/>
+        <source>&amp;Close Bugreport...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.py" line="145"/>
+        <source>Close the current Bugreport.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.py" line="146"/>
+        <source>Ctrl+L</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.py" line="147"/>
+        <source>New &amp;WNPP Bugreport...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.py" line="148"/>
+        <source>Create a new WNPP Bugreport.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.py" line="149"/>
+        <source>Ctrl+W</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.py" line="150"/>
+        <source>Clear</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.py" line="151"/>
+        <source>Clear.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.py" line="152"/>
+        <source>&amp;Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.py" line="153"/>
+        <source>Quit Reportbug-NG.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.py" line="154"/>
+        <source>Ctrl+Q</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.py" line="155"/>
+        <source>&amp;Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.py" line="156"/>
+        <source>Change Reportbug-NG&apos;s settings.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.py" line="157"/>
+        <source>About Reportbug NG</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.py" line="158"/>
+        <source>About Qt</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>RngGui</name>
+    <message>
+        <location filename="rnggui.py" line="210"/>
+        <source>About Reportbug-NG</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="rnggui.py" line="210"/>
+        <source>Copyright (C) 2007-2009 Bastian Venthur &lt;venthur at debian org&gt;
+
+Homepage: http://reportbug-ng.alioth.debian.org
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="rnggui.py" line="224"/>
+        <source>About Qt</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SubmitDialog</name>
+    <message>
+        <location filename="submitdialog.py" line="140"/>
+        <source>Bug report details</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="submitdialog.py" line="141"/>
+        <source>Please add details about the bug report you want to report.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="submitdialog.py" line="142"/>
+        <source>&amp;Package:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="submitdialog.py" line="143"/>
+        <source>&amp;Version:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="submitdialog.py" line="144"/>
+        <source>&amp;WNPP Bugreport</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="submitdialog.py" line="145"/>
+        <source>Short Package &amp;Description:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="submitdialog.py" line="146"/>
+        <source>WNPP &amp;Action:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="submitdialog.py" line="147"/>
+        <source>Normal Bugreport</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="submitdialog.py" line="148"/>
+        <source>&lt;b&gt;Please enter a short summary. Severity and Tags are optional.&lt;/b&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="submitdialog.py" line="149"/>
+        <source>&amp;Summary:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="submitdialog.py" line="150"/>
+        <source>S&amp;everity:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="submitdialog.py" line="151"/>
+        <source>Lorem Ipsum. Lorem Ipsum. Lorem Ipsum. Lorem Ipsum. Lorem Ipsum. Lorem Ipsum. Lorem Ipsum. Lorem Ipsum. Lorem Ipsum. Lorem Ipsum. Lorem Ipsum. Lorem Ipsum. Lorem Ipsum. Lorem Ipsum. Lorem Ipsum. Lorem Ipsum. Lorem Ipsum. Lorem Ipsum. Lorem Ipsum. Lorem Ipsum.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="submitdialog.py" line="152"/>
+        <source>&amp;Tags:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="submitdialog.py" line="153"/>
+        <source>A patch or some other easy procedure for fixing the bug is included in the bug logs. If there&apos;s a patch, but it doesn&apos;t resolve the bug adequately or causes some other problems, this tag should not be used.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="submitdialog.py" line="154"/>
+        <source>patch</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="submitdialog.py" line="155"/>
+        <source>This bug is relevant to the localisation of the package.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="submitdialog.py" line="156"/>
+        <source>l10n</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="submitdialog.py" line="157"/>
+        <source>This bug describes a security problem in a package (e.g., bad permissions allowing access to data that shouldn&apos;t be accessible; buffer overruns allowing people to control a system in ways they shouldn&apos;t be able to; denial of service attacks that should be fixed, etc). Most security bugs should also be set at critical or grave severity.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="submitdialog.py" line="158"/>
+        <source>security</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="submitdialog.py" line="159"/>
+        <source>Miscellaneous</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="submitdialog.py" line="160"/>
+        <source>Send additional bugscript information if available (recommended)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="submitdialog.py" line="161"/>
+        <source>Show package specific information before sending bugreport if available (recommended)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
